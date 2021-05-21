2e18e4f64f0fb2be579f01761f7ebe86613ee651
Paul Chin Jr <pchinjr@gmail.com>
2021-05-21 14:19:11 -0400

    static bucket env

--- src/http/get-images/index.js
+++ src/http/get-images/index.js
@@ -2,27 +2,11 @@ const arc = require('@architect/functions')
 
 exports.handler = arc.http.async(route)
 
-let image = `<img src=${arc.static('1.jpeg')}>`
-
+let image = arc.static('1.jpeg')
+console.log(process.env.ARC_STATIC_BUCKET)
 async function route (req) {
   return {
     statusCode: 200,
-    headers: {
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
-      'content-type': 'text/html; charset=utf8'
-    },
-    body: `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Architect</title>
-<body>
-<h1>Praise Cage</h1>
-${image}
-</body>
-</html>
-`
+    json: { data: { image } }
   }
 }
