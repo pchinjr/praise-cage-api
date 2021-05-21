b27d26976bca7387c2aaa88c73b19eb27f588c45
Paul Chin Jr <pchinjr@gmail.com>
2021-05-21 14:36:46 -0400

    json is here

--- src/http/get-images/index.js
+++ src/http/get-images/index.js
@@ -2,28 +2,14 @@ const arc = require('@architect/functions')
 
 exports.handler = arc.http.async(route)
 
-let image = `<img src=${arc.static('1.jpeg')}>`
+let image = `${arc.static('1.jpeg')}>`
 
 async function route (req) {
   return {
     statusCode: 200,
-    headers: {
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
-      'content-type': 'text/html; charset=utf8'
-    },
-    body: `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Architect</title>
-<body>
-<h1>Praise Cage</h1>
-${image}
-${process.env.BEGIN_STATIC_ORIGIN}
-</body>
-</html>
-`
+    json:
+          {image: `${process.env.BEGIN_STATIC_ORIGIN}/1.jpeg`}
+
+
   }
 }
