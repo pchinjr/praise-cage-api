06000b3f880fe041b761909f2a66028aa44e7c10
Paul Chin Jr <pchinjr@gmail.com>
2021-05-21 14:23:07 -0400

    reimplementing image

--- src/http/get-images/index.js
+++ src/http/get-images/index.js
@@ -2,11 +2,27 @@ const arc = require('@architect/functions')
 
 exports.handler = arc.http.async(route)
 
-let image = arc.static('1.jpeg')
-console.log(process.env.ARC_STATIC_BUCKET)
+let image = `<img src=${arc.static('1.jpeg')}>`
+
 async function route (req) {
   return {
     statusCode: 200,
-    json: { data: { image } }
+    headers: {
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
+      'content-type': 'text/html; charset=utf8'
+    },
+    body: `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Architect</title>
+<body>
+<h1>Praise Cage</h1>
+${image}
+</body>
+</html>
+`
   }
 }
